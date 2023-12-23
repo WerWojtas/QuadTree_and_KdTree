7054e13f031c712a490cc577cb113570b5078c45
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-12-23 20:39:38 +0100

    Created repo structure

---
+++ main.py
@@ -0,0 +1,9 @@
+from tests.KdTreeTests import KdTreeTests
+#from tests.QuadTreeTests import QuadTreeTests
+from visualizer.main import Visualizer
+from KdTree import KdTree
+#from QuadTree import QuadTree
+
+if __name__ == '__main__':
+    # TODO: Add command line arguments
+    pass
