7df9a8decd43d0f0622b4921c0ac2412dcf1515a
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-12-27 22:47:55 +0100

    comparator file created

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from tests.KdTreeTests import KdTreeTests
-#from tests.QuadTreeTests import QuadTreeTests
+from tests.TestManager import TestManager
 from visualizer.main import Visualizer
 from KdTree import KdTree
 #from QuadTree import QuadTree
+from utilities.Point import Point
+from utilities.Rectangle import Rectangle
 
 if __name__ == '__main__':
     # TODO: Add command line arguments
+    TestManager(KdTree).all_tests()
     pass
